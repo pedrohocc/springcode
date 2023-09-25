ff6d39716f1b91aa3d7512e45024e1ea7a22a0f5
Pedro Henrique <93086323+pedrohocc@users.noreply.github.com>
2023-09-25 08:50:01 -0300

    feat: getters e setters Endereço

--- src/main/java/projeto/clinica/models/Endereço.java
+++ src/main/java/projeto/clinica/models/Endereço.java
@@ -9,4 +9,47 @@ public class Endereço {
     private String complemento;
     private String cidade;
     private String uf;
+    
+    public String getLogradouro() {
+        return logradouro;
+    }
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+    public String getBairro() {
+        return bairro;
+    }
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+    public String getCep() {
+        return cep;
+    }
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+    public String getNumero() {
+        return numero;
+    }
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    public String getComplemento() {
+        return complemento;
+    }
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+    public String getCidade() {
+        return cidade;
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+    public String getUf() {
+        return uf;
+    }
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
 }
