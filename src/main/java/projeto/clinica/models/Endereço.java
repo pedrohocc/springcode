fa21f58ca680074a02060a403fce64cb7eafb39b
Pedro Henrique <93086323+pedrohocc@users.noreply.github.com>
2023-09-25 08:40:54 -0300

    feat: Criação "embeddable" endereço

---
+++ src/main/java/projeto/clinica/models/Endereço.java
@@ -0,0 +1,12 @@
+package main.java.projeto.clinica.models;
+
+@Embeddable
+public class Endereço {
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+}
